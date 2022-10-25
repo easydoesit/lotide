142f49d11799e15aab54555a2727390d7b967e47
easydoesit <michael.grills@gmail.com>
2022-10-25 05:48:35 +0000

    fixed Assert in all files.

--- head.js
+++ head.js
@@ -1,7 +1,10 @@
+//head function to get the first index of an array
 const head = function(argsArray) {
+  //make sure that there is something in the index.
   if (argsArray === undefined) {
     return `undefined`;
   }
+  //if there is something there then return it.
   return argsArray[0];
 };
 
