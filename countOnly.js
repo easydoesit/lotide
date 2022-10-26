6acc8f669d43fd44bb4601da356d82e3f0491ed6
easydoesit <michael.grills@gmail.com>
2022-10-26 05:09:26 +0000

    added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+  //emojies
+  const emoji = require('node-emoji');
+  const alert = emoji.get('x');
+  const pass = emoji.get('white_check_mark');
+  
+  //messages
+  const errorMessage = alert + alert + alert + " Assertion Failed:";
+  const successMessage = pass + pass + pass + " Assertion Passed:";
+
+  //comparison
+  if (actual === expected) {
+    return `${successMessage} ${actual} === ${expected}`;
+  } else {
+    return `${errorMessage} ${actual} !== ${expected}`;
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    //if the itemsToCount is true
+    if (itemsToCount[item]) {
+
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
+
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
+console.log(assertEqual(result1["Agouhanna"], undefined));
