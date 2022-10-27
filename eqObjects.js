3cb51f8886cf26a9bc99605148cbd38bf20c4562
easydoesit <michael.grills@gmail.com>
2022-10-27 23:58:51 +0000

    eqObjects now checks for undefind

--- eqObjects.js
+++ eqObjects.js
@@ -2,10 +2,16 @@
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  // Check for undefined
+  if (object1 === undefined || object2 === undefined) {
+    return false;
+  }
+
   //compare the objects sizes
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
+  
   // count through both objects and compare key-value pairs
   for (let key of Object.keys(object1)) {
     // if the value is an array
