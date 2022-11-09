2a3e157b5661ab0c5faeb43ebf7a2a577dfcea49
easydoesit <michael.grills@gmail.com>
2022-11-09 06:24:01 +0000

    refactored headTest.js to use Mocha Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,28 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const head = require("../head");
 
-assertEqual((head([5,6,7]) === 5),true);
-assertEqual((head(["Hello", "Lighthouse", "Labs"]) === "Hello"), true);
-assertEqual((head() === 5), false);
-assertEqual((head([5]) === 5), true);
+// mocha tests
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns false for undefined", () => {
+    assert.strictEqual(head(), "undefined");
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  
+
+});
