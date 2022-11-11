6a2098bf54fc8211b6da7a9257d21c7fe36d7699
easydoesit <michael.grills@gmail.com>
2022-11-11 02:07:39 +0000

    eqArrays works with objects and arrays numbers and strings.

--- eqArrays.js
+++ eqArrays.js
@@ -2,36 +2,45 @@ const eqObjects = require("./eqObjects");
 
 //eqArray Function takes two arrays and checks that they are equal
 const eqArrays = function(actual, expected) {
-
+  let trueOrFalse = true;
   // check to see that they are not underfined
   if (actual === undefined || expected === undefined) {
-    return false;
+    trueOrFalse = false;
   }
-
+  const returnTruth = function(actualA, expectedA) {
   // check to make sure they are arrays and not objects
-  if ((Object.prototype.toString.call(actual) !== "[object Array]") || (Object.prototype.toString.call(actual) !== "[object Array]")) {
-    return false;
-  }
-
-  // check to see if their lengths are equal
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  // count through the given array arguments and if there is anything that doesn't match up - return false
-  for (let i in actual) {
-    if (Object.prototype.toString.call(actual[i]) === "[object Object]") {
-      if (!eqObjects(actual, expected)) {
-        return false;
-      }
-      i++;
+    if ((Object.prototype.toString.call(actualA) !== "[object Array]") || (Object.prototype.toString.call(actualA) !== "[object Array]")) {
+      trueOrFalse = false;
     }
-    if (actual[i] !== expected[i]) {
-      return false;
+
+    // check to see if their lengths are equal
+    if (actualA.length !== expectedA.length) {
+      trueOrFalse = false;
     }
-  }
+
   
-  return true;
+    // count through the given array arguments and if there is anything that doesn't match up - return false
+    for (let i in actualA) {
+
+      console.log(actualA[i], expectedA[i]);
+
+
+      // check to see that the index is an object
+      if (Object.prototype.toString.call(actualA[i]) === "[object Object]") {
+        if (!eqObjects(actualA[i], expectedA[i])) {
+          trueOrFalse = false;
+        }
+        i++;
+      }
+      if (Array.isArray(actualA[i])) {
+        returnTruth(actualA[i], expectedA[i]);
+      } else if (actualA[i] !== expectedA[i]) {
+        trueOrFalse = false;
+      }
+    }
+  };
+  returnTruth(actual, expected);
+  return trueOrFalse;
 };
 
 module.exports = eqArrays;
